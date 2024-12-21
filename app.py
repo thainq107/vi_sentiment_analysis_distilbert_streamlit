cc577dae3831eb10b3cb7889076e084bf39ae401
ThaiNQ <110602193+thainq107@users.noreply.github.com>
2024-12-21 09:20:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ classifier = pipeline(
 def main():
   st.title('Sentiment Analysis')
   st.title('Model: DistilBERT. Dataset: NTC-SCV')
-  text_input = st.text_input("Sentence: ", "Đồ ăn ở quán này quá tệ luôn!")
+  text_input = st.text_input("Sentence: ", "đồ ăn ngon!")
   result = classifier(text_input)[0]
-  label = result['label']
-  score = result['score']
+  label = int(result['label'].split('_')[-1])
+  score = int(result['score'])
   st.success(f'Sentiment: {label} with {score:.2f} % probability.') 
 
 if __name__ == '__main__':
